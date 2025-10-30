1d054d54b8dbf44126bc290f2341bbf65580d0eb
SANMOSSA <santiago.murillo@utp.edu.co>
2025-10-30 14:58:04 -0500

    funciona pero sigue malas practicas

--- app.py
+++ app.py
@@ -1,40 +1,69 @@
 import re
 import os
+import json
 from urllib.parse import urlparse
 import gradio as gr
 from descargar import GestorDescargas
 
-categorias_opciones = ["Accion", "Animacion", "Comedia", "Historia", "Romance", "Terror"]
-categorias_anime = [
-    {"nombre": "DanDanDan", "url": "https://www.google.com"},
-    {"nombre": "Spyfamily", "url": "https://www.openai.com"},
-]
+# categorias_peliculas = ["Accion", "Animacion", "Comedia", "Historia", "Romance", "Terror"]
+# lista_animes = [
+#     {"nombre": "Lets Play Quest-darake no My Life", "url": "https://www3.animeflv.net/anime/lets-play-questdarake-no-my-life"},
+#     {"nombre": "Disney Twisted-Wonderland The Animation Episode of Heartslabyul", "url": "https://www3.animeflv.net/anime/disney-twistedwonderland-the-animation-episode-of-heartslabyul"},
+#     {"nombre": "Sakamoto days","url":"https://www3.animeflv.net/anime/sakamoto-days"}
+# ]
+
+RUTA_CONFIG = "Multimedia/config.json"
+categorias_peliculas = json.load(open(RUTA_CONFIG, "r", encoding="utf-8"))["categorias_peliculas"]
+lista_animes = json.load(open(RUTA_CONFIG, "r", encoding="utf-8"))["lista_animes"]
+
 
 gestor = GestorDescargas()
 
 
 def obtener_nombres_animes():
-    return [cat["nombre"] for cat in categorias_anime]
+    return [cat["nombre"] for cat in lista_animes]
 
 
 
 def buscar_episodios(nombre_anime):
     """Busca la categoría y devuelve la URL para demostrar funcionamiento."""
-    for cat in categorias_anime:
+    for cat in lista_animes:
+        yield barra_html(0), "Empezando búsqueda..."
         if cat["nombre"] == nombre_anime:
             # Aquí puedes poner el proceso que quieras que se ejecute con la URL
-            return f"Ejecutando proceso con URL: {cat['url']}"
-    return "Categoría no encontrada."
+            progreso = gestor.descargar_anime_con_progreso(cat['url'], nombre_anime)
+            for porcentaje,mensaje in progreso:
+                yield barra_html(porcentaje), mensaje
+            break
+        else:
+            yield barra_html(0), "Categoría no encontrada."
+    else:
+        yield barra_html(0), "Categoría no encontrada."
 
 
-def agregar_anime(nombre, url):
+def agregar_anime(url):
     """Agrega nueva anime a la lista y devuelve mensaje + lista actualizada."""
     # validar que no exista ya
-    if any(cat["nombre"] == nombre for cat in categorias_anime):
-        return f"La categoría '{nombre}' ya existe.", gr.Dropdown(choices=obtener_nombres_animes(),value=nombre)
+    existente = next((cat for cat in lista_animes if cat["url"] == url), None)
+    if existente:
+        return f"El anime ya existe.", gr.Dropdown(choices=obtener_nombres_animes(), value=existente["nombre"])
     
-    categorias_anime.append({"nombre": nombre, "url": url})
-    return f"Categoría '{nombre}' agregada correctamente.", gr.Dropdown(choices=obtener_nombres_animes(),value=nombre)
+    parsed = urlparse(url.strip())
+    if parsed.scheme not in ("http", "https") or not parsed.netloc:
+        return "URL no válida.", gr.Dropdown(choices=obtener_nombres_animes(),value=None)
+    
+    nombre = gestor.obtener_nombre_anime_desde_url(url)
+    if not nombre:
+        return "No se pudo obtener el nombre del anime desde la URL.", gr.Dropdown(choices=obtener_nombres_animes(),value=None)
+    lista_animes.append({"nombre": nombre, "url": url})
+    # Guardar en config.json
+    with open(RUTA_CONFIG, "r+", encoding="utf-8") as f:
+        config = json.load(f)
+        config["lista_animes"] = lista_animes
+        f.seek(0)
+        json.dump(config, f, indent=4, ensure_ascii=False)
+        f.truncate()
+    return f"Anime: '{nombre}' agregada correctamente.", gr.Dropdown(choices=obtener_nombres_animes(),value=nombre)
 
 
 
@@ -51,7 +80,7 @@ def descargar(categoria, link, nombre):
         return name
 
     # Validaciones
-    if not categoria or categoria not in categorias_opciones:
+    if not categoria or categoria not in categorias_peliculas:
         yield barra_html(0), "Categoría no válida."
         return
     parsed = urlparse(link.strip())
@@ -77,39 +106,39 @@ def barra_html(porcentaje):
     """
 
 with gr.Blocks() as demo:
-    with gr.Tab("Película"):
+    with gr.Tab("🎬 Película"):
         gr.Markdown("## 🎬 Descargar película")
 
-        categoria = gr.Dropdown(choices=categorias_opciones, label="Categoría")
+        categoria = gr.Dropdown(choices=categorias_peliculas, label="Categoría")
         link = gr.Textbox(label="Link de la película")
         nombre = gr.Textbox(label="Nombre del archivo (incluye extensión)")
 
-        barra_progreso = gr.HTML()
+        barra_progreso_peliculas = gr.HTML()
         salida = gr.Textbox(label="Estado", interactive=False)
 
         boton_descargar = gr.Button("Descargar")
-        boton_descargar.click(fn=descargar, inputs=[categoria, link, nombre], outputs=[barra_progreso, salida])
+        boton_descargar.click(fn=descargar, inputs=[categoria, link, nombre], outputs=[barra_progreso_peliculas, salida])
     
-    with gr.Tab("Animes"):
-        gr.Markdown("## 🎬 Descargar Animes de FLV")
+    with gr.Tab("🗾 Animes"):
+        gr.Markdown("## 🗾 Descargar Animes de FLV")
         with gr.Tab("Buscar episodios nuevos"):
             dropdown_cat = gr.Dropdown(
                 label="Selecciona un anime",
                 choices=obtener_nombres_animes(),
-                value=None,
+                value=obtener_nombres_animes()[0] if obtener_nombres_animes() else None,
                 interactive=True
             )
             btn_ejecutar = gr.Button("Buscar nuevos episodios")
-            salida_proceso = gr.Textbox(label="Resultado")
+            barra_progreso_anime = gr.HTML()
+            salida_proceso = gr.Markdown("Proceso...")
             
             btn_ejecutar.click(
                 buscar_episodios,
                 inputs=dropdown_cat,
-                outputs=salida_proceso
+                outputs={barra_progreso_anime,salida_proceso}
             )
             
         with gr.Tab("Agregar nuevo anime"):
-            nombre_input = gr.Textbox(label="Nombre del anime")
             url_input = gr.Textbox(label="URL del anime en AnimeFLV")
             btn_guardar = gr.Button("Guardar anime (NO DESCARGA)")
             salida_guardar = gr.Textbox(label="Estado")
@@ -117,8 +146,8 @@ with gr.Blocks() as demo:
             # Al guardar, se actualiza también el dropdown de la otra pestaña
             btn_guardar.click(
                 agregar_anime,
-                inputs=[nombre_input, url_input],
+                inputs=[url_input],
                 outputs=[salida_guardar, dropdown_cat]
         )
 
-demo.launch(server_name="0.0.0.0", server_port=8002)
+demo.launch(server_name="0.0.0.0", server_port=8002, share=True)
--- descargar.py
+++ descargar.py
@@ -2,6 +2,9 @@ import os
 import platform
 import requests
 from urllib.parse import urlparse
+from animeflv import AnimeFLV
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+import re
 
 class GestorDescargas:
     def __init__(self):
@@ -11,7 +14,34 @@ class GestorDescargas:
             self.ruta_base = "M:/codigo_propio/descargar_peliculas_series/peliculas_test"
         else:
             # Ruta base estándar para Linux/Docker
-            self.ruta_base = "/mnt/Multimedia"
+            self.ruta_base = "Multimedia"
+
+    def obter_video_cargado(url: str) -> str | None:
+        """
+        Abre la URL en un Chromium headless, espera hasta que exista
+        video#mainVideo source y devuelve su atributo src.
+        timeout_ms: tiempo máximo de espera en milisegundos (por defecto 15s).
+        """
+        try:
+            respuesta = requests.get(url, timeout=10)
+            if respuesta.status_code not in (200, 301, 302):
+                return None
+            with sync_playwright() as p:
+                browser = p.chromium.launch(headless=True)
+                page = browser.new_page()
+                page.goto(url)  # timeout para la navegación
+                # Esperar a que exista el source dentro del video
+                page.wait_for_selector("video")
+                src = page.get_attribute("video", "src")
+                browser.close()
+                return src
+        except PlaywrightTimeoutError:
+            # Timeout al cargar/esperar: devuelve None
+            return None
+        except Exception as e:
+            # Puedes loguear e imprimir el error si quieres
+            print("Error:", e)
+            return None
 
     def obtener_extension_real(self, url):
         """
@@ -23,7 +53,6 @@ class GestorDescargas:
         ext = os.path.splitext(parsed.path)[1].lower()
         if ext:
             return ext
-
         # 2. Intentar desde Content-Type con una solicitud HEAD
         try:
             head = requests.head(url, allow_redirects=True, timeout=10)
@@ -37,8 +66,121 @@ class GestorDescargas:
                 "video/webm": ".webm"
             }
             return mime_map.get(mime, "")
-        except Exception:
+        except Exception as e:
             return ""
+        
+
+    def obtener_nombre_anime_desde_url(self, url):
+        """
+        Extrae el nombre del anime desde la URL de AnimeFLV.
+        Ej: https://www3.animeflv.net/anime/nombre-del-anime -> nombre-del-anime
+        """
+        respuesta = requests.get(url)
+        if respuesta.status_code not in (200, 301, 302):
+            return None
+        with AnimeFLV() as aflv:
+            id_anime = url.split("/")[-1]
+            while True:
+                try:
+                    anime = aflv.get_anime_info(id_anime)
+                    nombre_limpio = re.sub(r'[^a-zA-Z0-9 ]', '', anime.title)
+                    return nombre_limpio
+                except Exception:
+                    pass
+        
+    def descargar_anime_con_progreso(self, url, nombre_anime):
+        id_anime = url.split("/")[-1]
+        ruta_categoria = os.path.join(self.ruta_base, "Animes", nombre_anime)
+        os.makedirs(ruta_categoria, exist_ok=True)
+        episodios_descargados = {}
+        episodios_fallidos = {}
+        servidores_alternos = {}
+        with AnimeFLV() as aflv:
+            while True:
+                try:
+                    anime = aflv.get_anime_info(id_anime)
+                    break
+                except Exception:
+                    pass
+            total_eps = len(anime.episodes)
+            eps_procesados = 0
+            anime.episodes.reverse()
+            for episodio in anime.episodes:
+                if episodio.id > 12:
+                    eps_procesados += 1
+                    continue
+                nombre_episodio = f"{nombre_anime.replace(' ', '.')}S01.E{str(episodio.id).zfill(3)}"
+                ruta_episodio = os.path.join(ruta_categoria, f"{nombre_episodio}")
+                servidores_descarga = aflv.get_links(id_anime, episodio.id)
+                descargado = False
+                for servidor in servidores_descarga:
+                    if servidor.server == "Stape":
+                        video_src = GestorDescargas.obter_video_cargado(servidor.url)
+                        if video_src:
+                            if video_src.startswith("//"):
+                                video_src = "https:" + video_src
+                            ext_real = self.obtener_extension_real(video_src)
+                            if not ext_real:
+                                episodios_fallidos[episodio.id] = "No se pudo determinar la extensión del archivo."
+                                continue
+                            ruta_final = ruta_episodio + ext_real
+                            
+                            try:
+                                with requests.get(video_src, stream=True) as r:
+                                    r.raise_for_status()
+                                    total = int(r.headers.get('Content-Length', 0))
+                                    if os.path.exists(ruta_final) and os.path.getsize(ruta_final) == total:
+                                        eps_procesados += 1
+                                        porcentaje = eps_procesados / total_eps * 100
+                                        mensaje = f"✅ Episodio {episodio.id} ya descargado ({eps_procesados}/{total_eps})"
+                                        descargado = True
+                                        yield porcentaje, mensaje
+                                        continue
+                                    descargado_bytes = 0
+                                    with open(ruta_final, 'wb') as f:
+                                        for chunk in r.iter_content(chunk_size=1024*1024):
+                                            if chunk:
+                                                f.write(chunk)
+                                                descargado_bytes += len(chunk)
+                                                if total:
+                                                    porcentaje = (eps_procesados + descargado_bytes / total) / total_eps * 100
+                                                    episodio_actual = f"{episodio.id}/{total_eps}"
+                                                    megas_descargadas =f"({descargado_bytes / (1024*1024):.2f}/{total / (1024*1024):.2f} MB)"
+                                                    mensaje = f"⬇️ Descargando episodio {episodio_actual} {megas_descargadas}"
+                                                    yield porcentaje, mensaje
+                                episodios_descargados[episodio.id] = ruta_final
+                                eps_procesados += 1
+                                porcentaje = eps_procesados / total_eps * 100
+                                mensaje = f"✅ Episodio {episodio.id} descargado ({eps_procesados}/{total_eps})"
+                                yield porcentaje, mensaje
+                                descargado = True
+                            except requests.RequestException as e:
+                                episodios_fallidos[episodio.id] = f"Error al descargar: {str(e)}"
+                            break
+                    else:
+                        if episodio.id not in servidores_alternos:
+                            servidores_alternos[episodio.id] = ""
+                        servidores_alternos[episodio.id] += f"{servidor.server}: {servidor.url}\n"
+                if not descargado:
+                    mensaje = f"❌ Episodio {episodio.id} fallido"
+                    eps_procesados += 1
+                    porcentaje = eps_procesados / total_eps * 100
+                    episodios_fallidos[episodio.id] = "No se pudo descargar desde Stape."
+                    yield porcentaje, mensaje
+            mensaje_final = f"**Descargados:** {eps_procesados-len(episodios_fallidos)} de {total_eps} episodios.\n"
+            if episodios_fallidos:
+                mensaje_final += f"**Fallidos:** {len(episodios_fallidos)}\n\n"
+                mensaje_final += "**Servidores alternos:**\n"
+                for ep_id, enlaces in servidores_alternos.items():
+                    if ep_id in episodios_fallidos:
+                        mensaje_final += f"- Episodio `{ep_id}`:\n"
+                        for enlace in enlaces.strip().split('\n'):
+                            if enlace:
+                                mensaje_final += f"  - {enlace}\n"
+            else:
+                mensaje_final += "**Todos los episodios descargados correctamente.**"
+            yield 100, mensaje_final
+
 
     def descargar_pelicula_con_progreso(self, categoria, url, nombre_base):
         """
@@ -59,24 +201,26 @@ class GestorDescargas:
         # Nombre final con extensión real
         nombre_final = os.path.basename(nombre_base.strip()) + ext_real
         ruta_archivo = os.path.join(ruta_categoria, nombre_final)
-
-        try:
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                total = int(r.headers.get('Content-Length', 0))
-                descargado = 0
-
-                with open(ruta_archivo, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024*1024):  # 1 MB
-                        if chunk:
-                            f.write(chunk)
-                            descargado += len(chunk)
-                            if total:
-                                porcentaje = descargado / total * 100
-                                yield porcentaje
-                            else:
-                                yield 0
-
+        if os.path.exists(ruta_archivo):
             yield 100
-        except requests.RequestException as e:
-            yield 0
+        else:
+            try:
+                with requests.get(url, stream=True) as r:
+                    r.raise_for_status()
+                    total = int(r.headers.get('Content-Length', 0))
+                    descargado = 0
+
+                    with open(ruta_archivo, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=1024*1024):  # 1 MB
+                            if chunk:
+                                f.write(chunk)
+                                descargado += len(chunk)
+                                if total:
+                                    porcentaje = descargado / total * 100
+                                    yield porcentaje
+                                else:
+                                    yield 0
+
+                yield 100
+            except requests.RequestException as e:
+                yield 0
